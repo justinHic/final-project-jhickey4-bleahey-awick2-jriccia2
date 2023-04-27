237883af7da44ee0ace23f531c0700d944a17ad5
Brendan <brendan_leahey@brown.edu>
2023-04-26 20:35:44 -0400

    changing keys

--- src/pages/api/spotify/testVerifier.js
+++ src/pages/api/spotify/testVerifier.js
@@ -166,9 +166,10 @@ function getUserData() {
     });
 }
 
-const client_id = "94e44d9212c746d0be764f24a8e61079";
-//const redirect_uri = "https://localhost:3000/new"; // Your redirect uri
-const redirect_uri = "https://www.google.com";
+const client_id = "d4f1fb65364d48f38e76c1d7c26da3ae";
+//const client_id = "94e44d9212c746d0be764f24a8e61079";
+const redirect_uri = "https://localhost:3000"; // Your redirect uri
+//const redirect_uri = "https://www.google.com";
 
 // Restore tokens from localStorage
 // let access_token = localStorage.getItem("access_token") || null;
