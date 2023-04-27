cefb156fbd4f67eb7213c22933e72bc425531c3d
justinHic <justinhickey22@yahoo.com>
2023-04-27 13:07:15 -0400

    temp

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import Image from "next/image";
-import { Inter } from "next/font/google";
 import { ChangeEvent, ChangeEventHandler, useEffect, useState } from "react";
 import { Metronome } from "../scripts/metronome";
 import localFont from "next/font/local";
