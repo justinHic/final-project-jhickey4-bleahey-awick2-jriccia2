818c9ab68a7d27c94b4bcd89da355b4d40636eb4
joey <joseph_ricciardulli@brown.edu>
2023-05-05 15:16:28 -0400

    done with tests for songs

--- __tests__/backend.test.tsx
+++ __tests__/backend.test.tsx
@@ -14,7 +14,6 @@ afterEach(() => {
 
 describe("/api/spotify/songs", () => {
   test("returns success", async () => {
-    
     fetchMock.mockResponseOnce(JSON.stringify(mockResponse));
     const { req, res } = createMocks({
       method: "GET",
@@ -31,6 +30,117 @@ describe("/api/spotify/songs", () => {
     await songHandler(req, res);
     expect(res.statusCode).toBe(200);
     expect(res._getData()).toEqual('{"uris":["string"],"result":"success!"}');
+  });
 
+  test("bad query - no bpm", async () => {
+    fetchMock.mockResponseOnce(JSON.stringify(mockResponse));
+    const { req, res } = createMocks({
+      method: "GET",
+      query: {
+        genres: "hip-hop",
+        numsongs: "5",
+        access_token: localStorage.getItem("access_token"),
+        height: "72",
+        male: "true",
+        hr: "",
+      },
+    });
+    await songHandler(req, res);
+    expect(res.statusCode).toBe(405);
+  });
+
+  test("bad query - no genres", async () => {
+    fetchMock.mockResponseOnce(JSON.stringify(mockResponse));
+    const { req, res } = createMocks({
+      method: "GET",
+      query: {
+        bpm: "220",
+        numsongs: "5",
+        access_token: localStorage.getItem("access_token"),
+        height: "72",
+        male: "true",
+        hr: "",
+      },
+    });
+    await songHandler(req, res);
+    expect(res.statusCode).toBe(405);
+  });
+
+  test("bad query - no numsongs", async () => {
+    fetchMock.mockResponseOnce(JSON.stringify(mockResponse));
+    const { req, res } = createMocks({
+      method: "GET",
+      query: {
+        bpm: "220",
+        genres: "hip-hop",
+        access_token: localStorage.getItem("access_token"),
+        height: "72",
+        male: "true",
+        hr: "",
+      },
+    });
+    await songHandler(req, res);
+    expect(res.statusCode).toBe(405);
+  });
+
+  test("bad query - no access_token", async () => {
+    fetchMock.mockResponseOnce(JSON.stringify(mockResponse));
+    const { req, res } = createMocks({
+      method: "GET",
+      query: {
+        bpm: "220",
+        genres: "hip-hop",
+        numsongs: "5",
+        height: "72",
+        male: "true",
+        hr: "",
+      },
+    });
+    await songHandler(req, res);
+    expect(res.statusCode).toBe(405);
+  });
+
+  test("bad query - no height", async () => {
+    fetchMock.mockResponseOnce(JSON.stringify(mockResponse));
+    const { req, res } = createMocks({
+      method: "GET",
+      query: {
+        bpm: "220",
+        genres: "hip-hop",
+        numsongs: "5",
+        access_token: localStorage.getItem("access_token"),
+        male: "true",
+        hr: "",
+      },
+    });
+    await songHandler(req, res);
+    expect(res.statusCode).toBe(405);
+  });
+
+  test("bad query - no male", async () => {
+    fetchMock.mockResponseOnce(JSON.stringify(mockResponse));
+    const { req, res } = createMocks({
+      method: "GET",
+      query: {
+        bpm: "220",
+        genres: "hip-hop",
+        numsongs: "5",
+        access_token: localStorage.getItem("access_token"),
+        height: "72",
+        hr: "",
+      },
+    });
+    await songHandler(req, res);
+    expect(res.statusCode).toBe(405);
+  });
+
+  test("bad query", async () => {
+    fetchMock.mockResponseOnce(JSON.stringify(mockResponse));
+    const { req, res } = createMocks({
+      method: "GET",
+      query: {},
+    });
+    await songHandler(req, res);
+    expect(res.statusCode).toBe(405);
   });
 });
--- src/pages/api/spotify/songs.ts
+++ src/pages/api/spotify/songs.ts
@@ -70,47 +70,6 @@ export default async function songHandler(
   }
 }
 
-function isTrackObject(track: any): track is TrackObject {
-  if (!("album" in track)) return false;
-  if (!("artists" in track)) return false;
-  if (!("available_markets" in track)) return false;
-  if (!("disc_number" in track)) return false;
-  if (!("duration_ms" in track)) return false;
-  if (!("explicit" in track)) return false;
-  if (!("external_ids" in track)) return false;
-  if (!("external_urls" in track)) return false;
-  if (!("href" in track)) return false;
-  if (!("id" in track)) return false;
-  if (!("name" in track)) return false;
-  if (!("popularity" in track)) return false;
-  if (!("preview_url" in track)) return false;
-  if (!("track_number" in track)) return false;
-  if (!("type" in track)) return false;
-  if (!("uri" in track)) return false;
-  if (!("is_local" in track)) return false;
-  return true
-}
-
-export interface TrackObject {
-  album: object,
-  artists: object[],
-  available_markets: string[],
-  disc_number: number,
-  duration_ms: number,
-  explicit: boolean,
-  external_ids: object,
-  external_urls: object,
-  href: string,
-  id: string,
-  name: string,
-  popularity: number,
-  preview_url: string,
-  track_number: number,
-  type: string,
-  uri: string,
-  is_local: boolean,
-}
-
 function isString(item: any): item is string {
   if (item === undefined) return false;
   if (Array.isArray(item)) return false;
