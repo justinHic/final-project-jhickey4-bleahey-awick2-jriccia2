013e0e08f8140bca0a51a04cd1408b65df8710df
justinHic <justinhickey22@yahoo.com>
2023-05-07 19:52:46 -0400

    502 error fixed

--- src/pages/api/spotify/queue.ts
+++ src/pages/api/spotify/queue.ts
@@ -15,7 +15,45 @@ export default async function queueHandler(
       },
       position_ms: 0,
     });
-    console.log(body)
+    console.log(body);
+    let tries = 5;
+    const fetchResult: any = async () => {
+      const result = await fetch(
+        "https://api.spotify.com/v1/me/player/play?device_id=" + device_id,
+        {
+          method: "PUT",
+          headers: {
+            Authorization: "Bearer " + access_token,
+            "Content-Type": "application/json",
+          },
+          body: body,
+        }
+      );
+      if (result.status === 502 && tries > 0) {
+        console.log("thi");
+        tries -= 1;
+        return fetchResult();
+      } else if (result.status !== 202) {
+        console.log(result.status);
+        console.log(result.statusText);
+        return result.json();
+      } else {
+        return JSON.stringify({ result: "success" });
+      }
+    };
+
+    await fetchResult()
+      .then((info: any) => {
+        console.log("hit");
+        console.log(info);
+        res.status(200).json({ result: info });
+      })
+      .catch((err: any) => {
+        console.log(err);
+        res.status(400).end();
+      });
+
+    /*
     const result = await fetch(
       "https://api.spotify.com/v1/me/player/play?device_id=" + device_id,
       {
@@ -29,20 +67,25 @@ export default async function queueHandler(
     )
       .then((resp) => {
         if (resp !== undefined) {
+          if (resp.status === 502) {
+          }
           resp
             .json()
-            .then((json) => console.log(json))
+            .then((json) => {
+              console.log(json);
+            })
             .catch((err) => {
               console.log(err);
               res.status(400).end();
-            })
-        };
+            });
+        }
       })
       .catch((err) => {
+        console.log("hit");
         console.log(err);
         res.status(405).end();
-      })
-    res.status(200).end();
+      });
+    res.status(200).end();*/
   } else {
     res.status(400).end();
   }
