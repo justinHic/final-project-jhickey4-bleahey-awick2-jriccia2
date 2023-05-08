c4d14e3cfbd028023e51f58fb6b6858b353c627a
alexjwick <alexjwick03@gmail.com>
2023-05-08 18:19:15 -0400

    deleted duplicates that git made for some reason

--- src/components/HeightInput 2.tsx
+++
@@ -1,6 +0,0 @@
-import { Dispatch, SetStateAction } from "react";
-
-export interface HeightInputProps {
-  setInches: Dispatch<SetStateAction<Number>>;
-  setFeet: Dispatch<SetStateAction<Number>>;
-}
