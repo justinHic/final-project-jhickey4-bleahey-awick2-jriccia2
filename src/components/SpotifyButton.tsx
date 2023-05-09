6543f7822a24491e99a9ce6d28bd37716dbcd145
alexjwick <alexjwick03@gmail.com>
2023-05-09 00:38:51 -0400

    improved typing of web player code and generate playlist button only shows once web player is loaded

--- src/components/SpotifyButton.tsx
+++ src/components/SpotifyButton.tsx
@@ -1,23 +1,48 @@
 import { NextRouter } from "next/router";
 
+/**
+ * Represents the action that the SpotifyButton will take when clicked.
+ */
 export enum SpotifyButtonAction {
+  /**
+   * Represents the action to login to Spotify.
+   */
   Login = "Login to Spotify",
+
+  /**
+   * Represents the action to logout of Spotify.
+   */
   Logout = "Logout of Spotify",
 }
 
+/**
+ * The props for the SpotifyButton component.
+ * @property {SpotifyButtonAction} action The action that the SpotifyButton will take when clicked.
+ * @property {NextRouter} router The router object.
+ */
 interface SpotifyButtonProps {
   action: SpotifyButtonAction;
   router: NextRouter;
 }
 
 /**
- *
+ * The interface for the response from the Spotify API.
+ * @property {string} url The URL to redirect the user to.
  */
 interface RedirectURL {
   url: string;
 }
 
-export default function SpotifyButton(props: SpotifyButtonProps) {
+/**
+ * A component that allows the user to login to Spotify.
+ * @param {SpotifyButtonProps} props The props for the SpotifyButton.
+ * @returns {JSX.Element} A SpotifyButton component.
+ */
+export default function SpotifyButton(props: SpotifyButtonProps): JSX.Element {
+  /**
+   * Redirects the user to the Spotify login and authorization page.
+   * @returns {void}
+   */
   function login(): void {
     fetch("/api/spotify/verify")
       .then((res) => {
@@ -30,12 +55,20 @@ export default function SpotifyButton(props: SpotifyButtonProps) {
       });
   }
 
-  function logout() {
+  /**
+   * Logs the user out of Spotify.
+   * @returns {void}
+   */
+  function logout(): void {
     localStorage.clear();
     sessionStorage.clear();
     props.router.push("/");
   }
 
+  /**
+   * Handles the click event for the SpotifyButton.
+   * @param {SpotifyButtonAction} action The action that the SpotifyButton will take when clicked.
+   */
   function handleClick(action: SpotifyButtonAction): void {
     switch (action) {
       case SpotifyButtonAction.Login:
